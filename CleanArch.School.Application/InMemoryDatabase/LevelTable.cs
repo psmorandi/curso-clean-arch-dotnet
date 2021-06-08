111dd40e9479bc5b49f22318360f7d9e4962c156
Paulo Morandi <paulomorandi@mobiltec.com.br>
2021-06-07 21:33:16 -0300

    Removed unused classes.

--- CleanArch.School.Application/InMemoryDatabase/LevelTable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CleanArch.School.Application.InMemoryDatabase
-{
-    public class LevelTable
-    {
-        public string Code { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-    }
-}
