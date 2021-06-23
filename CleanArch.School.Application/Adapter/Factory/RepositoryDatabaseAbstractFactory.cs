c67ffd3f7a382fe12968b7b86522a39e344ea95d
Paulo Morandi <paulomorandi@mobiltec.com.br>
2021-06-23 15:44:49 -0300

    Added module database repo to abstract factory.

--- CleanArch.School.Application/Adapter/Factory/RepositoryDatabaseAbstractFactory.cs
+++ CleanArch.School.Application/Adapter/Factory/RepositoryDatabaseAbstractFactory.cs
@@ -8,14 +8,16 @@
     public class RepositoryDatabaseAbstractFactory : IRepositoryAbstractFactory
     {
         private readonly LevelRepositoryDatabase levelRepository;
+        private readonly ModuleRepositoryDatabase moduleRepository;
         public RepositoryDatabaseAbstractFactory(PostgresConnectionPool connectionPool)
         {
             this.levelRepository = new LevelRepositoryDatabase(connectionPool);
+            this.moduleRepository = new ModuleRepositoryDatabase(connectionPool);
         }
 
         public ILevelRepository CreateLevelRepository() => this.levelRepository;
 
-        public IModuleRepository CreateModuleRepository() => throw new System.NotImplementedException();
+        public IModuleRepository CreateModuleRepository() => this.moduleRepository;
 
         public IClassroomRepository CreateClassroomRepository() => throw new System.NotImplementedException();
 
