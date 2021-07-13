961bf9d1697c6c9aa992874d23d290494ad1094d
Paulo Morandi <paulomorandi@mobiltec.com.br>
2021-07-13 13:44:34 -0300

    Added extensions methods to some formatting.

---
+++ CleanArch.School.WebApp/Client/Extensions/DecimalExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArch.School.WebApp.Client.Extensions
+{
+    using System.Globalization;
+
+    public static class DecimalExtensions
+    {
+        public static string FormatAsBrlCurrency(this decimal value) => value.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+    }
+}
---
+++ CleanArch.School.WebApp/Client/Extensions/EnrollStatusExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArch.School.WebApp.Client.Extensions
+{
+    using Domain.Entity;
+
+    public static class EnrollStatusExtensions
+    {
+        public static string AsString(this EnrollStatus enrollStatus) => enrollStatus switch
+        {
+            EnrollStatus.Active => "Enrolled",
+            EnrollStatus.Cancelled => "Cancelled",
+            _ => "Unknown"
+        };
+    }
+}
